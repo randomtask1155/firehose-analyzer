df131c56fc779cef45587b74b361fcbeaa38ac27
randomtask1155 <dlynch@pivotal.io>
2020-01-03 09:12:24 -0600

    bump version to 1.1.1

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 1,
 			Minor: 1,
-			Build: 0,
+			Build: 1,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
